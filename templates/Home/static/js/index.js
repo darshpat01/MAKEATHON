65878a4a497deb4da87d28a9eb77a3541dfb20b0
darshpat01 <darshpat01@gmail.com>
2021-01-13 23:37:17 +0700

    final template

--- templates/Home/static/js/index.js
+++ templates/Home/static/js/index.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+var countDownDate = new Date("Feb 23, 2021 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -11,12 +11,12 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
     
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor(distance/1000);
 
-  console.log(days,hours,minutes);
+  // console.log(days,hours,minutes);
   console.log(seconds);
     
   // Output the result in an element with id="demo"
@@ -24,12 +24,11 @@ var x = setInterval(function() {
   // + minutes + "m " + seconds + "s ";
   // var str = `<h1>${days} d </h1>` + hours + "h " + minutes + "m " +seconds + "s "; 
   var str=""; 
-  var time = String(days) + hours + minutes + seconds;
+  var time = String(seconds);
   
     for(var i =0;i<time.length; i++){
-    str+=`<span class="timerspan">${time[i]}</span>`
-    console.log(str); 
-
+    str+=`<span class="timerspan col-md-3">${time[i]}</span>`
+    // console.log(str); 
     }
     
 
